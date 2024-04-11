648f80cfcc2d76485a6d207e58f24bf62760d3a6
FabioIngenito <fabio.ingenito@hotmail.com>
2024-04-11 10:12:48 -0300

    chore: create routes

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import http from "http";
 
+const PORT = 3000;
+
+//A partir da URL Raiz:
+const rotas = {
+    "/" : "Curso de Node.js",
+}
+
 const server = http.createServer((req, res) => {
-    res.
+    res.writeHead(200, {"Content-type" : "text/plain"});
+    res.end(rotas[req.url])
+});
+
+server.listen(PORT, () => {
+    console.log("Hello, world!")
 })
