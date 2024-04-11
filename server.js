6bfec1d18f76de1d12819992c4995e1aadacfdcf
Fabio Ingenito <fabio.ingenito@hotmail.com>
2024-04-11 14:09:21 -0300

    Atualização forçada

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-import http from 'http';
+import app from './src/app.js';
 
 const PORT = 3000;
 
-//A partir da URL Raiz:
-const rotas = {
-  '/': 'Servidor - OKAY',
-  '/cachorros': 'Entrei na rota cachorro',
-  '/racas': 'Entrei na rota raca',
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-type': 'text/plain' });
-  res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
-  console.log('Hello, world!');
+app.listen(PORT, () => {
+  console.log('Hello, world! - EXPRESS');
 });
